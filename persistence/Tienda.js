4684db12c13e22c5130df531d99854a7e1eca587
Pablo Bassil <pabcubus@gmail.com>
2017-03-22 14:55:05 -0500

    no message

--- persistence/Tienda.js
+++ persistence/Tienda.js
@@ -2,6 +2,7 @@ function init(Sequelize, connection) {
 	Tienda = connection.define('tienda', {
 		ciudadId: Sequelize.BIGINT,
 		codigo: Sequelize.STRING,
+		nombre: Sequelize.STRING,
 		presupuesto_tope: Sequelize.BIGINT,
 		presupuesto_global: Sequelize.BIGINT,
 		creado: Sequelize.DATE,
